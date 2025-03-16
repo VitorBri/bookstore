9cbe54d524f836db79fd54f2c30e560648cefcf8
Vitor Brito <joaovitor16611661@gmail.com>
2025-03-15 23:37:26 -0300

    integration serializers/tests

---
+++ product/serializers/product_serializer.py
@@ -0,0 +1,17 @@
+from rest_framework import  serializers
+
+from product.models.product import Product
+from product.serializers.category_serializer import CategorySerializer
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Product
+        fields = [
+            "title",
+            "description",
+            "price",
+            "active",
+            "category"
+        ]
